d5a4f5969da22eccf924b817bd14e5c91aa8c23e
=josh91hickman <josh91hickman@gmail.com>
2017-07-28 21:29:15 -0700

    fix on createUser

--- imports/startup/server/accounts/createUser.js
+++ imports/startup/server/accounts/createUser.js
@@ -1,23 +1,47 @@
-import { moment } from 'meteor/momentjs:moment';
 
-
-Accounts.onCreateUser((options, user) => {
-  let timeStamp = moment().format('MMMM Do YYYY');
-  user.createdAt = timeStamp
-  user.verified  = false;
-
-  if (!user.services.twitter) {
-    user.profile_img = "./defaultAvatar.jpeg";
-  }
-
-  Meteor.call('sendVerificationLink', user, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      console.log('sent');
-    }
-  });
+ import { moment } from 'meteor/momentjs:moment';
+ import { Random } from 'meteor/random';
+ 
+   
+  Accounts.onCreateUser((options, user) => {
+     
+    const timeStamp = moment().format('MMMM Do YYYY');
+    let email;
+    let username;
+    let verified = false;
+    let randomnizer = Random.id(6);
+    let profile_img = "./defaultAvatar.jpeg";
   
-  return user;
+    
+      const userToCreate = user;
+  
+      if (user.services.facebook) {
+          email = user.services.facebook.email;
+          username = `${user.services.facebook.name}_${randomnizer}`;
+        }
+  
+      if (user.services.google) {
+          email = user.services.google.email;
+          username = `${user.services.google.name}_${randomnizer}`;
+      
+            if (user.services.google.picture) {
+                profile_img = user.services.google.picture;
+          
+                }
+      
+            if (user.services.google.verified_email) {
+                verified = true;
+              }
+        }
+  
+      if (options.profile) userToCreate.profile = options.profile;
+  
+      if (!user.services.password) {
+          userToCreate.emails = [{ address: email, verified: verified }];
+          userToCreate.username = username;
+        }
+    userToCreate.profile_img = profile_img;
+    userToCreate.socials = { };
+    userToCreate.createdAt = timeStamp;
+    return userToCreate;
 });
